3619c5910642dfccfb21309c0497a97e216ddc71
cs2338139 <cs2338139@gmail.com>
2022-12-02 16:07:51 +0800

    ENTER

---
+++ components/ProjectLayoutItem/ProjectLayout.vue
@@ -0,0 +1,60 @@
+<script setup>
+import PartItem from '~~/components/ProjectLayoutItem/PartItem.vue'
+import PartItem2 from '~~/components/ProjectLayoutItem/PartItem2.vue'
+import PartItemNull from '~~/components/ProjectLayoutItem/PartItemNull.vue';
+import Item from '~~/components/ProjectLayoutItem/Item.vue';
+import Popup from '~~/components/ProjectLayoutItem/Popup.vue';
+
+defineProps({
+    img: String,
+});
+
+const popupInfo = reactive({
+    img: '',
+    name: '',
+    word: ''
+})
+
+const popupEnable = ref(false);
+
+function OpenPopupPanel(img, name, word) {
+    popupEnable.value = true;
+    document.body.style.overflow = "hidden";
+    popupEnable
+}
+
+function ClosePopupPanel() {
+    popupEnable.value = false;
+    document.body.style.overflow = "scroll";
+}
+
+defineExpose({
+    OpenPopupPanel
+})
+
+</script>
+
+<template>
+
+    <div>
+        <div class="aspect-video wrap-8 mb-14 w-full bg-gray-300"> </div>
+
+        <div class="wrap-6 mb-10">
+            <div class="mb-14 mt-0 ml-0 text-5xl">
+                <slot name="title"></slot>
+            </div>
+            <slot name="info"></slot>
+        </div>
+
+        <Popup @close="ClosePopupPanel" :img="popupInfo.img" v-if="popupEnable">
+            <template #name>{{ popupInfo.name }}</template>
+            <template #word>
+                {{ popupInfo.word }}</template>
+        </Popup>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
